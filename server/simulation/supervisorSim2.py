2f63643d6c21f390abc171c079203652aabfc0c3
anubhav <anubhav.balodhi@gmail.com>
2020-06-22 17:01:38 +0530

    dummy chage to check push to server

--- server/simulation/supervisorSim2.py
+++ server/simulation/supervisorSim2.py
@@ -9,7 +9,7 @@ sys.path.insert(0, os.path.dirname(__file__))
 from entity import *
 
 '''
-Script simulates a drivers behaviour
+Script simulates a superVisor behaviour
 Run multiple instances of this to simulate multiple
 '''
 
