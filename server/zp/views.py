370a211f46d5a34f50f32ee4b1cfcd53edda6e1c
anubhav <anubhav.balodhi@gmail.com>
2020-07-21 19:11:38 +0530

    updated rental to send remaining amount, userTripGetStatus() to send rtype

--- server/zp/views.py
+++ server/zp/views.py
@@ -351,9 +351,9 @@ def userTripGetStatus(_dct, user):
                 #diffTime = (currTime - trip.stime).total_seconds() // 60 # minutes
                 #remHrs = diffTime - trip.hrs
                 #price = getRentPrice(trip.srcid,  trip.dstid, vehicle.vtype, trip.pmode, remHrs)
-                price = getTripPrice(trip)
+                price = getTripPrice(trip) - getRentPrice(trip.hrs)
                 #TODO get the remaining amount for the rental
-            ret.update(price)
+            ret['price'] = price
     else:
         ret = {'active': False}
 
