b4be8e44bc86c5a6cc398f2cf2f3777d58627d37
anubhav <anubhav.balodhi@gmail.com>
2020-08-08 12:02:26 +0530

    added delA.sh

--- server/simulation/superV.sh
+++ server/simulation/superV.sh
@@ -3,7 +3,6 @@ echo "Looking for incoming rental requests"
 curl -X POST https://api.villageapps.in:8090/sup-rent-check -d '{"auth":"sauth01"}'
 
 echo "\n"
-
 echo "Enter the trip id for assignment"
 read tid
 echo 'tid is: '$tid
@@ -14,13 +13,14 @@ echo 'van is: '$van
 
 curl -X POST https://api.villageapps.in:8090/sup-rent-vehicle-assign -d '{"auth":"sauth01","van":"'$veh'","tid":"'$tid'"}'
 
-
+echo "\n"
 echo "Enter the OTP"
 read otp
 echo 'otp is: '$otp
 curl -X POST https://api.villageapps.in:8090/sup-rent-start -d '{"auth":"sauth01","tid":"'$tid'","otp":"'$otp'"}'
 
 
+echo "\n"
 echo "Do you want to END the rental(y/n)?"
 read end
 
@@ -29,7 +29,7 @@ if [[ "$end" -eq 'y' ]]
 		curl -X POST https://api.villageapps.in:8090/sup-rent-end -d '{"auth":"sauth01","tid":"'$tid'"}'
 fi
 
-
+echo "\n"
 echo "Do you want to RETIRE the rental(y/n)?"
 read retire
 
