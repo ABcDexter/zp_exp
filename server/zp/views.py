8f4800996de5b3ccc34b56bac3d562680857749e
uday <udaykabirsingh@gmail.com>
2020-11-08 13:15:16 +0530

    debugged an in dct for authLocaitonUpdate()

--- server/zp/views.py
+++ server/zp/views.py
@@ -704,7 +704,7 @@ def authLocationUpdate(dct, entity):
     # Create or edit
     if len(qsLoc) == 0:
         recLoc = Location()
-        recLoc.an = dct['an'] if an in dct else '0'
+        recLoc.an = dct['an'] if 'an' in dct else '0'
     else:
         recLoc = qsLoc[0]
 
