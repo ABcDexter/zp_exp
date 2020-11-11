8c2cc1ccc183fa3dc72d7c680e66b737d7733b82
anubhav_win <anubhav.balodhi@gmail.com>
2020-11-11 11:04:59 +0530

    debugged agentDeliveryCheck()

--- server/zp/view/deliver.py
+++ server/zp/view/deliver.py
@@ -844,7 +844,7 @@ def agentDeliveryCheck(_dct, agent):
         if nTime or nDist:
             if nDist < 10_000:  # 10 kms radius
                 print({'did': deli['id'], 'srcland':deli['srcland'], 'dstland':deli['dstland']})
-                delis.append({'did': deli['id'], 'srcland':deli['srcland'], 'dstland':deli['dstland']})
+                delis.append({'did': deli['id'], 'srcland':deli['srcland'], 'dstland':deli['dstland'], 'srclat':deli['srclat'], 'srclng':deli['srlng']})
 
     ret = {} if len(delis) == 0 else {'did': delis[0]['did'], 'srclat': delis[0]['srclat'],'srclng': delis[0]['srclng'],'srcland':delis[0]['srcland'], 'dstland':delis[0]['dstland']}
     ret.update({'count': len(delis)})
