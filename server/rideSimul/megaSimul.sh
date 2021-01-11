920ab9d36aa388bd60ce1a92decd05b0f085992c
Abcdexter <anubhav.balodhi@gmail.com>
2021-01-11 11:40:00 +0530

    updated mysql.txt and megaSimul.sh for user limit testing

--- server/rideSimul/megaSimul.sh
+++ server/rideSimul/megaSimul.sh
@@ -37,42 +37,43 @@ sleep 10
 
 echo "#################### DOING ####################" | tee -a "$log_file"
 echo "deleting users and drivers..."
-echo "delete from user; "| mysql --user=$ZP_DB_USER --password=$ZP_DB_PASSWD $ZP_DB_NAME
-echo "delete from driver; "| mysql --user=$ZP_DB_USER --password=$ZP_DB_PASSWD $ZP_DB_NAME
-echo "delete from vehicle;" | mysql --user=$ZP_DB_USER --password=$ZP_DB_PASSWD $ZP_DB_NAME
+#echo "delete from user; "| mysql --user=$ZP_DB_USER --password=$ZP_DB_PASSWD $ZP_DB_NAME
+#echo "delete from driver; "| mysql --user=$ZP_DB_USER --password=$ZP_DB_PASSWD $ZP_DB_NAME
+#echo "delete from vehicle;" | mysql --user=$ZP_DB_USER --password=$ZP_DB_PASSWD $ZP_DB_NAME
+echo "no need as already inserted!"
 
 ####################################
 
 echo "creating $user_count users..."
-user_iter=1
-while [ $user_iter -le $user_count ]
-  do
-    echo "INSERT INTO user(an, pn, auth, tid, pid, age, gdr, name, hs, dl) VALUES('$user_iter', '97000000$user_iter', 'auth$user_iter', -1, FLOOR(RAND()*(10-5+1)+1),  25, 'MALE', 'John $user_iter',  'UK', 'UK-1234567890$user_iter');"
-    ((user_iter++))
-  done | mysql --user=$ZP_DB_USER --password=$ZP_DB_PASSWD $ZP_DB_NAME
-echo "created $user_count users."
+#user_iter=1
+#while [ $user_iter -le $user_count ]
+#  do
+#    echo "INSERT INTO user(an, pn, auth, tid, pid, age, gdr, name, hs, dl, did, mark) VALUES('$user_iter', '97000000$user_iter', 'auth$user_iter', -1, FLOOR(RAND()*(10-5+1)+1),  25, 'MALE', 'John $user_iter',  'UK', 'UK-1234567890$user_iter', '-1', '0.0');"
+#    ((user_iter++))
+#  done | mysql --user=$ZP_DB_USER --password=$ZP_DB_PASSWD $ZP_DB_NAME
+echo "already created $user_count users."
 
 ####################################
 
 echo "creating $driver_count drivers..."
-driver_iter=1
-while [ $driver_iter -le $driver_count ]
-  do
-    echo "INSERT INTO driver (an, pn, auth, dl, mode, tid, pid, name, age, gdr, hs) VALUES('$driver_iter', '98000000$driver_iter', 'dauth$driver_iter', 'UK01234567891$driver_iter', 'OF', -1,FLOOR(RAND()*(10-5+1)+1), 'driver0$driver_iter', 30, 'm', 'uk');"
-    ((driver_iter++))
-  done | mysql --user=$ZP_DB_USER --password=$ZP_DB_PASSWD $ZP_DB_NAME
-echo "created $driver_count drivers."
+#driver_iter=1
+#while [ $driver_iter -le $driver_count ]
+#  do
+#    echo "INSERT INTO driver (an, pn, auth, dl, mode, tid, pid, name, age, gdr, hs, mark, van) VALUES('$driver_iter', '98000000$driver_iter', 'dauth$driver_iter', 'UK01234567891$driver_iter', 'OF', -1,FLOOR(RAND()*(10-5+1)+1), 'driver0$driver_iter', 30, 'm', 'uk', '0.0', '-1');"
+#    ((driver_iter++))
+#  done | mysql --user=$ZP_DB_USER --password=$ZP_DB_PASSWD $ZP_DB_NAME
+echo "already created $driver_count drivers."
 
 #####################################
 
 echo "creating $vehicle_count vechicles..."
-vehicle_iter=1
-while [ $vehicle_iter -le $vehicle_count ]
-  do
-    echo "INSERT INTO vehicle(regn, dist, hrs, pid, vtype, an, tid) VALUES('reg$vehicle_iter', '0', '0', FLOOR( 1 + RAND() * 7 ),  FLOOR( 1 + RAND() * 3 ), '20$vehicle_iter',  '-1');"
-    ((vehicle_iter++))
-  done | mysql --user=$ZP_DB_USER --password=$ZP_DB_PASSWD $ZP_DB_NAME
-echo "created $vehicle_count vehicles."
+#vehicle_iter=1
+#while [ $vehicle_iter -le $vehicle_count ]
+#  do
+#    echo "INSERT INTO vehicle(regn, dist, hrs, pid, vtype, an, tid, dan, mark) VALUES('reg$vehicle_iter', '0', '0', FLOOR( 1 + RAND() * 7 ),  FLOOR( 1 + RAND() * 3 ), '20$vehicle_iter',  '-1' '-1', '0.0');"
+#    ((vehicle_iter++))
+#  done | mysql --user=$ZP_DB_USER --password=$ZP_DB_PASSWD $ZP_DB_NAME
+echo "already created $vehicle_count vehicles."
 
 #####################################
 
