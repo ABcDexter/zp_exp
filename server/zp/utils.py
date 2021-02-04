adec615ee897e6a9cfcfe97e63f00e663d4c7a23
Abcdexter <anubhav.balodhi@gmail.com>
2021-02-04 13:13:55 +0530

    added encoding/decoding of servitor auth

--- server/zp/utils.py
+++ server/zp/utils.py
@@ -1405,6 +1405,9 @@ def extract_name_from_pin(address_or_postalcode, data_type='json'):
     return townName
 
 
+#####################
+# Sending mail
+
 def sendTripInvoiceMail(tripType, userEmail, userName, tripId, tripDate, tripTime, tripPrice, tripCGST, tripSGST, tripTotal ):
     '''
     sends mail to the user with Trip invoice
@@ -1541,3 +1544,51 @@ def sendDeliveryInvoiceMail(deliveryType, userEmail, userName, deliveryId, deliv
     server = smtplib.SMTP(SENDER_SERVER)
     server.sendmail(FROM, TO, message.as_string())
     server.quit()
+
+
+#############################
+#  Encryption / Decryption
+
+
+def encode(num, alphabet):
+    """
+    Encode a positive number into Base X and return the string.
+
+    Arguments:
+    - `num`: The number to encode
+    - `alphabet`: The alphabet to use for encoding
+    """
+    if num == 0:
+        return alphabet[0]
+    arr = []
+    arr_append = arr.append  # Extract bound-method for faster access.
+    _divmod = divmod  # Access to locals is faster.
+    base = len(alphabet)
+    while num:
+        num, rem = _divmod(num, base)
+        arr_append(alphabet[rem])
+    arr.reverse()
+    return str(''.join(arr))
+
+
+def decode(string, alphabet=settings.BASE62):
+    """
+    Decode a Base X encoded string into the number
+
+    Arguments:
+    - `string`: The encoded string
+    - `alphabet`: The alphabet to use for decoding
+
+    """
+    base = len(alphabet)
+    strlen = len(string)
+    num = 0
+
+    idx = 0
+    for char in string:
+        power = (strlen - (idx + 1))
+        num += alphabet.index(char) * (base ** power)
+        idx += 1
+
+    return str(num)
+
