392429e10fbbcff674f5eb6c2ea2bd7a9ad5c204
Abcdexter <anubhav.balodhi@gmail.com>
2021-04-05 14:33:40 +0530

    testing github webhook

--- server/api/settings.py
+++ server/api/settings.py
@@ -26,8 +26,9 @@ SECRET_KEY = '+8^8@0w0p1)6yr-g)!3l04&qp-ggjwz@9f_-fvpm_qta0g&36r'
 # NOTE : If this is turned False, then the static files won't work!!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "api.villageapps.in", "media.villageapps.in", "159.65.144.72", "api.zippe.in",
-                 "86d9a981a121.ngrok.io"]  # ngrok http http://127.0.0.1:9999
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "api.villageapps.in", "media.villageapps.in", "159.65.144.72",
+                 "api.zippe.in",
+                 "86d9a981a121.ngrok.io"]  # ngrok http http://127.0.0.1:9999 on the server
 
 PROFILE_PHOTO_DIR = '/srv/data/static/dp'
 AADHAAR_DIR = '/srv/data/aadhaar/'
